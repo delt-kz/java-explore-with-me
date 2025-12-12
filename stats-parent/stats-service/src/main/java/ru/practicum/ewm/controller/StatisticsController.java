3d841f7107f7ff9512b009f1ca06216adda9be15
delt-kz <yerkebulansarsenbek@yandex.kz>
2025-12-12 09:35:20 +0500

    Обновить значение параметра unique по умолчанию на false в StatisticsController

--- stats-parent/stats-service/src/main/java/ru/practicum/ewm/controller/StatisticsController.java
+++ stats-parent/stats-service/src/main/java/ru/practicum/ewm/controller/StatisticsController.java
@@ -28,7 +28,7 @@ public class StatisticsController {
     public List<StatsDto> stats(@RequestParam String start,
                           @RequestParam String end,
                           @RequestParam(required = false) List<String> uris,
-                          @RequestParam(defaultValue = "true") Boolean unique) {
+                          @RequestParam(defaultValue = "false") Boolean unique) {
         String decodedStart = URLDecoder.decode(start, StandardCharsets.UTF_8);
         String decodedEnd = URLDecoder.decode(end, StandardCharsets.UTF_8);
         return service.getStats(decodedStart, decodedEnd, uris, unique);
